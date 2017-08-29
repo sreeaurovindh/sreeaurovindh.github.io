14d10371375cda85a501175058b11e935091005b
sreeaurovindh <sreeaurovindh@gmail.com>
2017-08-29 13:39:39 -0700

    Creation of Website

---
+++ js/portfolioGrid.js
@@ -0,0 +1,46 @@
+//THESE FUNCTIONS CONTROL THE CONTENT ON THE PORTFOLIO GRID
+function displayAll(){
+  $('.flex-item').hide(1000);
+  $('.flex-item').show(1000);
+}
+function displayResearch(){
+    $('.flex-item').hide(1000);
+    $('.classification').show(1000);
+}
+
+function displayExp(){
+  $('.flex-item').hide(1000);
+  $('.experimentation').show(1000);
+}
+function displayProb(){
+  $('.flex-item').hide(1000);
+  $('.pd').show(1000);
+}
+function displayStat(){
+  $('.flex-item').hide(1000);
+  $('.statTests').show(1000);
+}
+function displayNLP(){
+  $('.flex-item').hide(1000);
+  $('.expertRecommendation').show(1000);
+  $('.p2identification').show(1000);
+}
+function displayDB(){
+  $('.flex-item').hide(1000);
+  $('.sql').show(1000);
+  $('.nosql').show(1000);
+}
+function displayML(){
+  $('.flex-item').hide(1000);
+  $('.randomf').show(1000);
+  $('.svm').show(1000);
+  $('.bagging').show(1000);
+  $('.boosting').show(1000);
+  $('.neuralnets').show(1000);
+}
+function displayBDA(){
+  $('.flex-item').hide(1000);
+  $('.hive').show(1000);
+  $('.hadoop').show(1000);
+  $('.spark').show(1000);
+}
